34e4fd76071394746bfe9f49fcfc5e45dc807e81
congming02 <congmingw02@gmail.com>
2022-05-20 13:06:59 +1000

    v3
    v3

---
+++ wwwroot/wp-content/plugins/document-emberdder/admin/inc/metabox.php
@@ -0,0 +1,60 @@
+<?php
+// Control core classes for avoid errors
+add_action('plugins_loaded', function(){
+  if( class_exists( 'CSF' ) ) {
+
+    //
+    // Set a unique slug-like ID
+    $prefix = 'ppv';
+  
+    //
+    // Create a metabox
+    CSF::createMetabox( $prefix, array(
+      'title'     => 'Configuration',
+      'post_type' => 'ppt_viewer',
+    ) );
+  
+    //
+    // Create a section
+    CSF::createSection( $prefix, array(
+      'title'  => '',
+      'fields' => apply_filters( 'ppv_pro_metabox', array(
+        array(
+          'id'    => 'doc',
+          'type'  => 'upload',
+          'title' => 'Document',
+        ),
+        array(
+            'id' => 'width',
+            'type' => 'dimensions',
+            'title' => 'Width',
+            'height' => false,
+            'default' => ['width' => '100', 'unit' => '%']
+        ),
+        array(
+            'id' => 'height',
+            'type' => 'dimensions',
+            'title' => 'Height',
+            'width' => false,
+            'default' => ['height' => 600, 'unit' => 'px']
+        ),
+        array(
+            'id' => 'showName',
+            'type' => 'switcher',
+            'title' => 'Show file name on top',
+            'default' => 0
+        ),
+        [
+          'id' => 'download',
+          'type' => 'switcher',
+          'title' => __('Show downlaod button', 'ppv'),
+          'desc' => 'is not available for google drive and dropbox',
+          'default' => 1
+        ],
+        
+        ) )
+    ) );
+
+    apply_filters('ppv_settings', '');
+}  
+});
